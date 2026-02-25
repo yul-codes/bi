2e5b16b02c5907f9467e08e93342e01ee0d8cb72
Yulia K. <137112656+yul-codes@users.noreply.github.com>
2026-02-25 19:23:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.title("📊 Аналитика из Superset")
 
 # Встраиваем дашборд
 st.components.v1.iframe(
-    src="http://localhost:8088/superset/dashboard/p/12/?standalone=1",
+    src="src="http://host.docker.internal:8088/superset/dashboard/p/ваш_id/?standalone=1",
     width=1200,
     height=800,
     scrolling=True
